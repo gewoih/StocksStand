d96121d84ceb6f6f2431aca5ecb04e0a295be4a2
Никита Раненко <nranenko@bk.ru>
2022-01-18 13:12:25 +0300

    Добавлена библиотека MaterialDesign Добавлена форма для создания Секторов Изменен общий дизайн приложения Добавлено контекстное меню

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,8 +1,15 @@
-﻿using StocksStand.DataContext;
+﻿using StocksStand.Commands;
+using StocksStand.DataContext;
 using StocksStand.Models;
+using StocksStand.Models.Abstractions;
 using StocksStand.Repositories;
 using StocksStand.ViewModels.Base;
+using StocksStand.Views.Windows;
+using System;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
 
 namespace StocksStand.ViewModels
 {
@@ -11,11 +18,16 @@ namespace StocksStand.ViewModels
 		#region Constructor
 		public MainViewModel()
 		{
+			this.ChangeSelectedItemCommand = new RelayCommand(OnChangeSelectedItemCommandExecuted, CanChangeSelectedItemCommandExecute);
+			this.ShowNewSectorWindowCommand = new RelayCommand(OnShowNewSectorWindowCommandExecuted, CanShowNewSectorWindowCommandExecute);
+			this.AddSectorCommand = new RelayCommand(OnAddSectorCommandExecuted, CanAddSectorCommandExecute);
+
 			this.Sectors = new ObservableCollection<Sector>(new SectorsRepository(new BaseDataContext()).GetAll());
 		}
 		#endregion
 
 		#region Properties
+		//Список всех секторов
 		private ObservableCollection<Sector> _Sectors;
 		public ObservableCollection<Sector> Sectors
 		{
@@ -23,16 +35,96 @@ namespace StocksStand.ViewModels
 			set => Set(ref _Sectors, value);
 		}
 
-		private ObservableCollection<Industry> _Industries;
-		public ObservableCollection<Industry> Industries
+		private Sector _SelectedSector;
+		public Sector SelectedSector
 		{
-			get => _Industries;
-			set => Set(ref _Industries, value);
+			get => _SelectedSector;
+			set => Set(ref _SelectedSector, value);
+		}
+
+		private Industry _SelectedIndustry;
+		public Industry SelectedIndustry
+		{
+			get => _SelectedIndustry;
+			set => Set(ref _SelectedIndustry, value);
+		}
+
+		private AFinancialInstrument _SelectedFinancialInstrument;
+		public AFinancialInstrument SelectedFinancialInstrument
+		{
+			get => _SelectedFinancialInstrument;
+			set => Set(ref _SelectedFinancialInstrument, value);
+		}
+
+		//Объект нового сектора
+		private Sector _NewSector;
+		public Sector NewSector
+		{
+			get => _NewSector;
+			set => Set(ref _NewSector, value);
+		}
+
+		//Форма создания/редактирования сектора
+		private NewSectorView _NewSectorView;
+		public NewSectorView NewSectorView
+		{
+			get => _NewSectorView;
+			set => Set(ref _NewSectorView, value);
 		}
 		#endregion
 
 		#region Commands
+		public ICommand ChangeSelectedItemCommand { get; }
+		private bool CanChangeSelectedItemCommandExecute(object p) => true;
+		private void OnChangeSelectedItemCommandExecuted(object p)
+		{
+			//Проверка выбранного элемента TreeView
+			if (p is Sector)
+				this.SelectedSector = (Sector)p;
+			else if (p is Industry)
+				this.SelectedIndustry = (Industry)p;
+			else if (p is AFinancialInstrument)
+				this.SelectedFinancialInstrument = (AFinancialInstrument)p;
+		}
 
+		//Отображение окна для добавления нового Sector
+		public ICommand ShowNewSectorWindowCommand { get; }
+		private bool CanShowNewSectorWindowCommandExecute(object p) => true;
+		private void OnShowNewSectorWindowCommandExecuted(object p)
+		{
+			//Инициализируем новый сектор
+			this.NewSector = new Sector();
+
+			//Инициализируем и вызываем форму для создания нового сектора
+			this.NewSectorView = new NewSectorView(this);
+			this.NewSectorView.ShowDialog();
+		}
+
+		//Добавление нового Sector
+		public ICommand AddSectorCommand { get; }
+		private bool CanAddSectorCommandExecute(object p) => true;
+		private void OnAddSectorCommandExecuted(object p)
+		{
+			//Пустое ли имя сектора?
+			if (this.NewSector.Name != String.Empty)
+			{
+				SectorsRepository SectorsRepository = new SectorsRepository(new BaseDataContext());
+				//Проверяем есть ли в базе сектор с таким названием (без учета регистра)
+				if (SectorsRepository.GetAll().FirstOrDefault(c => c.Name.ToLower() == this.NewSector.Name.ToLower()) == null)
+				{
+					//Если такой сектор не найден - создаем новый
+					this.Sectors.Add(SectorsRepository.Create(this.NewSector));
+
+					//Закрываем форму создания сектора
+					this.NewSectorView.Close();
+					MessageBox.Show($"Сектор '{this.NewSector.Name}' успешно создан.");
+				}
+				else
+					MessageBox.Show("Сектор с таким названием уже существует.");
+			}
+			else
+				MessageBox.Show("Заполните имя сектора!");
+		}
 		#endregion
 	}
 }
---
+++ Views/Windows/NewSectorView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using StocksStand.ViewModels;
+using System.Windows;
+
+namespace StocksStand.Views.Windows
+{
+	/// <summary>
+	/// Логика взаимодействия для CreateSectorView.xaml
+	/// </summary>
+	public partial class NewSectorView : Window
+	{
+		public NewSectorView(MainViewModel MainViewModel)
+		{
+			InitializeComponent();
+
+			this.DataContext = MainViewModel;
+		}
+	}
+}
